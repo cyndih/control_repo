d3b64fbe36f2c367b9aa766a06705333310116c0
cyndih <chartmann8@gmail.com>
2019-11-01 10:03:56 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDomPrHyCr7OYYbhkpTkQV9NaJ+fo5tmFY03DDon3I2H+fP7UYN8888JsK69d301v7PYYeAaQmMhy5Tr2lvEQaOonrOeTMaGOytVWl9oRolPzTL8/AgKHjtlfwiTXfd3CwqIiNzuOs/XKcRtsdp6UiCpuOygW+5/bHhzkN8jmvYLSZX41wZdFX2qb10QsJ4Ibhx1x/ZqsAXZVQTwtjxNNFPx6a7o5tjSP9BF1TKi8o9zVF6d0Z7spCatCTvybH52rzQJV45B1Ggsi5KvXvCIOqd7N5wWJDPrfXEea6d1dCDQO+lsgJZxfkgapoalI9kD299d4hFjE2ZqIvbIv2oC94f',
 	}  
 }
