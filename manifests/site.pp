46cd67fb109208aa0f4712d4f2b12331ce6814c1
cyndih <chartmann8@gmail.com>
2019-11-01 09:30:53 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,4 +2,15 @@ node default {
 }
 node 'cos3.business.unc.edu' {
   include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => $fqdn,
+    }
+}
+node /*web/ {
+  include role:app_server
+}
+
+node /*db/ {
+  include role:db_server
 }
