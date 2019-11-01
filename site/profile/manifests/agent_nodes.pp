554e7eb06fd277d97ac0c896abab111c308fc9ed
cyndih <chartmann8@gmail.com>
2019-11-01 10:12:51 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web1.business.unc.edu':}
   dockeragent::node {'db1.business.unc.edu':}
+  host {'web1.business.unc.edu':
+    ensure => present,
+    ip     => '172.18.0.2'
+  }
+  host {'db1.business.unc.edu':
+    ensure => present,
+    ip     => '172.18.0.3'
+  }
 }
